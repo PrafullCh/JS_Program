5362e23d6e35d0b44023ad7fdbacf9c55720bd1e
prafullCh <prafullchavan0123@gmail.com>
2019-08-30 11:21:40 +0530

    Java Programs

--- Packages/Package1/Threads/Thread1.java
+++ Packages/Package1/Threads/Thread1.java
@@ -11,7 +11,7 @@ class Thread1 implements Runnable{
 		for(int i=1;i<10;i++)
 		{
 			try{
-			System.out.println(i*num);
+			System.out.println(+(i*num));
 		        Thread.sleep(1000);
 			}catch(Exception e)
 			{
