e98e5f2b8a2279e44f49f043459c286289d166aa
prafullCh <prafullchavan0123@gmail.com>
2019-09-20 11:31:35 +0530

    done

---
+++ Packages/Package1/Sample2/Sample.java
@@ -0,0 +1,8 @@
+ //import ./Sample1.FileFromSample1;
+
+class Sample{
+    public static void main(String[] args) {
+        FileFromSample1 sp = new FileFromSample1();
+        sp.table(4);
+    }
+}
---
+++ Packages/Package1/Threads/ImportingClass/Example1.java
@@ -0,0 +1,7 @@
+// import Sample1.FileFromSample1;
+
+class Example1{
+    public static void main(String[] args) {
+      System.out.println("Hello");  
+    }
+}
